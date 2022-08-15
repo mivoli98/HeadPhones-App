298c21a558c8df6d098c12920034315ecf7b1e99
mivoli98 <mivoli98@tamu.edu>
2022-08-15 13:02:33 -0500

    first commit

---
+++ sanity_ecommerce/schemas/brands.js
@@ -0,0 +1,28 @@
+export default {
+    name: 'brand',
+    title: 'Brands',
+    type: 'document',
+    fields: [{
+            name: 'image',
+            title: 'Image',
+            type: 'image',
+            options: {
+                hotspot: true,
+            }
+        },
+        {
+            name: 'name',
+            title: 'Name',
+            type: 'string',
+        },
+        {
+            name: 'slug',
+            title: 'Slug',
+            type: 'slug',
+            option: {
+                source: 'name',
+                maxLength: 90,
+            },
+        },
+    ],
+};
